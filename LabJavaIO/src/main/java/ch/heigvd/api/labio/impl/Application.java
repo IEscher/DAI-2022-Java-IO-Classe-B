f900106910cbc45126f0d4c7373266fa9f56ca74
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-10-03 17:53:49 +0200

    Fetch call storeQuote
    Co-authored-by: Ian Escher <54139119+IEscher@users.noreply.github.com>

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/Application.java
@@ -78,6 +78,7 @@ public class Application {
        *  Add the missing line which stores the content of the quote in a file with
        *  the name "quote-i.utf8" where 'i' is the number of the file.
        */
+      storeQuote(quote, "quote-" + i + ".utf8");
 
       LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
       for (String tag : quote.getTags()) {
